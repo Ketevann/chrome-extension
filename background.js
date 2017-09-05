b7e8c4fa1c3f576587211567698878fbfc706d9f
Ketevann <ketevan.tsin@gmail.com>
2017-09-05 19:43:51 -0400

    working on having mutliple tabs"

--- background.js
+++ background.js
@@ -1,29 +1,20 @@
 var xhr = new XMLHttpRequest();
-
-
-
-
+var data = {}
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
-  if(message.text == "getStuff"){
+  if(message.text == "get"){
     xhr.open("GET", `https://newsapi.org/v1/articles\?source\=${message.source}\&sortBy\=top\&apiKey\=e17dc00315af4fd1b8a5857184fd1cc6`, false);
 xhr.send();
 
 var result = xhr.responseText;
-
-console.log(typeof result)
-console.log(JSON.parse(result))
-
-
-    sendResponse({type:"test", data: JSON.parse(result) });
+ data.result = JSON.parse(result) ;
+    //sendResponse({type:"test", data: JSON.parse(result) });
   }
 });
-
-
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
-  if(message.text == "hey"){
+  if(message.text == "getStuff"){
 
 
 
-    sendResponse({type:"test", data:'so' });
+    sendResponse({type:"test", data: data.result });
   }
 });
--- popup.js
+++ popup.js
@@ -1,13 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  // code...
+
+
+
 myForm = document.getElementsByName('Form')[0];
 
 console.log(myForm)
 //alert('*******************')
 
 myForm.onsubmit = function (evt) {
-  evt.preventDefault()
+ // evt.preventDefault()
   console.log(evt.target.src.value, 'biiild')
-
-chrome.runtime.sendMessage({text:"get", data: evt.target.src.value},function(reponse){
+//myForm.select.value
+chrome.runtime.sendMessage({text:"get", source: evt.target.src.value},function(reponse){
   //This is where the stuff you want from the background page will be
   console.log(reponse,' respose!!!')
 if(reponse.type == "test2")
@@ -21,3 +27,4 @@ if(reponse.type == "test2")
 
 
 
+})
