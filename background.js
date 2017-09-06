e8989ff3fdf9c2d27e14afb4f1ceba5e971ebcfa
Ketevann <ketevan.tsin@gmail.com>
2017-09-06 19:01:49 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 var xhr = new XMLHttpRequest();
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  console.log(message, 'messi')
   if (message.text == "getNews") {
     console.log('get!!')
     xhr.open("GET", `https://newsapi.org/v1/articles\?source\=${message.source}\&sortBy\=top\&apiKey\=${API_KEY}`, false);
