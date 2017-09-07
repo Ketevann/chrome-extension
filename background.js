276b2e873dccecd14b093b5e1ffa2df90481d3a4
Ketevann <ketevan.tsin@gmail.com>
2017-09-06 20:19:34 -0400

    Update background.js

--- background.js
+++ background.js
@@ -5,7 +5,6 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     xhr.open("GET", `https://newsapi.org/v1/articles\?source\=${message.source}\&sortBy\=top\&apiKey\=${API_KEY}`, false);
     xhr.send();
     var result = xhr.responseText;
-    data.result = JSON.parse(result);
     sendResponse({ type: "sendNews", data: JSON.parse(result) });
   }
 });
